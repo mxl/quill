bff626c52b6a5c505bb2627255e99d7fb291a3af
fwbrasil <fwbrasil@gmail.com>
2016-07-30 19:11:54 -0700

    wip

--- quill-core/src/main/scala/io/getquill/lifting/LiftingMacro.scala
+++ quill-core/src/main/scala/io/getquill/lifting/LiftingMacro.scala
@@ -14,25 +14,32 @@ class LiftingMacro(val c: MacroContext) {
       }
   }
 
-  def regularEncoder[T](implicit t: WeakTypeTag[T]): Option[Tree] = { 
-    c.info(q"implictly[${c.prefix}.Encoder[$t]]".toString)
+  def regularEncoder[T](implicit t: WeakTypeTag[T]): Option[Tree] =
     c.typecheck(
       q"implicitly[${c.prefix}.Encoder[$t]]",
       silent = true) match {
         case EmptyTree => None
         case tree      => Some(tree)
       }
-  }
 
   def anyValEncoder[T](implicit t: WeakTypeTag[T]): Option[Tree] =
-    caseClassConstructor(t.tpe).map(_.paramLists.flatten).collect {
-      case param :: Nil =>
-        c.typecheck(q"""
-          ${c.prefix}.mappedEncoder(
-            ${c.prefix}.MappedEncoding((v: $t) => v.${param.name.toTermName}), 
-            implicitly[${c.prefix}.Encoder[${param.typeSignature}]]
-          )
-        """)
+    t.tpe.baseType(c.symbolOf[AnyVal]) match {
+      case false => None
+      case true =>
+        caseClassConstructor(t.tpe).map(_.paramLists.flatten).collect {
+          case param :: Nil =>
+            regularEncoder(c.WeakTypeTag(param.typeSignature)) match {
+              case Some(encoder) =>
+                c.typecheck(q"""
+                  ${c.prefix}.mappedEncoder(
+                    ${c.prefix}.MappedEncoding((v: $t) => v.${param.name.toTermName}), 
+                    $encoder
+                  )
+                """)
+              case None =>
+                c.fail(s"Can't encode the '${t.tpe}' because there's no encoder for '$param'.")
+            }
+        }
     }
 
   private def caseClassConstructor(t: Type) =
