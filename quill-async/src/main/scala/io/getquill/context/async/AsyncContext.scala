ce76cdd67d658c7f67555b54e9934d8c9cddbdf9
Michael Ledin <m.ledin@appheads.ru>
2016-08-12 17:33:25 +0300

    Replaced recursion-alike Future chaining with foldLeft.

--- quill-async/src/main/scala/io/getquill/context/async/AsyncContext.scala
+++ quill-async/src/main/scala/io/getquill/context/async/AsyncContext.scala
@@ -40,8 +40,8 @@ abstract class AsyncContext[D <: SqlIdiom, N <: NamingStrategy, C <: Connection]
 
   private def withConnection[T](f: Connection => Future[T])(implicit ec: ExecutionContext) =
     ec match {
-      case TransactionalExecutionContext(ec, conn) => f(conn)
-      case other                                   => f(pool)
+      case TransactionalExecutionContext(_, conn) => f(conn)
+      case other                                  => f(pool)
     }
 
   protected def extractActionResult[O](generated: Option[String], returningExtractor: RowData => O)(result: DBQueryResult): O
@@ -74,19 +74,16 @@ abstract class AsyncContext[D <: SqlIdiom, N <: NamingStrategy, C <: Connection]
     bindParams:         T => BindedStatementBuilder[List[Any]] => BindedStatementBuilder[List[Any]] = (_: T) => identity[BindedStatementBuilder[List[Any]]] _,
     generated:          Option[String]                                                              = None,
     returningExtractor: RowData => O                                                                = identity[RowData] _
-  )(implicit ec: ExecutionContext): List[T] => Future[List[O]] = {
-    def run(values: List[T]): Future[List[O]] =
-      values match {
-        case Nil =>
-          Future.successful(List())
-        case value :: tail =>
-          val (expanded, params) = bindParams(value)(new SqlBindedStatementBuilder).build(sql)
-          logger.info(expanded.toString)
-          withConnection(conn => conn.sendPreparedStatement(expandAction(expanded, generated), params(List())).map(extractActionResult(generated, returningExtractor)(_)))
-            .flatMap(r => run(tail).map(r +: _))
+  )(implicit ec: ExecutionContext): List[T] => Future[List[O]] =
+    _.foldLeft(Future.successful(List[O]())) { (future, value) =>
+      future.flatMap { seq =>
+        val (expanded, params) = bindParams(value)(new SqlBindedStatementBuilder).build(sql)
+        logger.info(expanded.toString)
+        withConnection(conn => conn.sendPreparedStatement(expandAction(expanded, generated), params(List()))
+          .map(extractActionResult(generated, returningExtractor)(_)))
+          .map(r => seq :+ r)
       }
-    run _
-  }
+    }
 
   def executeQuery[T](sql: String, extractor: RowData => T = identity[RowData] _, bind: BindedStatementBuilder[List[Any]] => BindedStatementBuilder[List[Any]] = identity)(implicit ec: ExecutionContext) = {
     val (expanded, params) = bind(new SqlBindedStatementBuilder).build(sql)
